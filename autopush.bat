4c1806e74611e158283f1fbb76c2eae3f677af47
rgn2698-wq <rgn2698@gmail.com>
2026-01-15 17:11:34 +0900

    20260115 Feat. tag

--- autopush.bat
+++ autopush.bat
@@ -1,16 +1,13 @@
 #!/bin/bash
 echo "자동 업로드"
 
-# 1. 땡겨오기
 git pull origin main
 
-# 2. 담기
 git add .
 
-# 3. 커밋하기
-git commit -m "Auto Update: $(date)"
+git commit -m "Auto Update"
 
-# 4. 쏘기
 git push origin main
 
 echo "=== 끝 ==="
+pause
